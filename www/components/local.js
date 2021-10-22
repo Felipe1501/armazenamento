c450d7cb0dcd6a2f15554d9452397ca520d67383
Felipe1501 <felipebsantos1501@gmail.com>
2021-10-22 09:23:39 +0900

    até o momento funcionando corretamente

--- www/components/local.js
+++ www/components/local.js
@@ -2,13 +2,15 @@
 var valor = 0;
 
 $(document).on("click", "#salvar", function(){
-  localStorage.setItem(valor, $("#nome").val());
+  var dados = {nome: $("#nome").val(), endereco: $("#endereco").val()};
+  localStorage.setItem(valor,JSON.stringify(dados));
   valor++;
 });
 
 $(document).on("click", "#buscar", function(){
-  var pessoa = localStorage.getItem($("#identificador").val());
-  $("#nmBusca").val(pessoa);
+  var pessoa = JSON.parse(localStorage.getItem($("#identificador").val()));
+  $("#nmBusca").val(pessoa.nome);
+  $("#enderecoBusca").val(pessoa.endereco);
 });
 
 $(document).on("click", "#limpar", function(){
@@ -17,12 +19,14 @@ $(document).on("click", "#limpar", function(){
 });
 
 $(document).on("click", "#remover", function(){
-  var pessoa = localStorage.removeItem()
+  var pessoa = localStorage.removeItem($("#identificador").val());
   limparCampos();
 });
 
 function limparCampos(){
   $("#nome").val("");
+  $("#endereco").val("");
   $("#identificador").val("");
   $("#nmBusca").val("");
+  $("#enderecoBusca").val("");
 }
